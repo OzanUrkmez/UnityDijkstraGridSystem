87e06e789b145fc3abbe1a019d7036038edb8b98
Ozan Ürkmez <arkantos250@gmail.com>
2019-11-23 19:28:19 +0300

    fixed breaking bugs

--- Grid.cs
+++ Grid.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using QuestryGameGeneral.PathFinding;
 using System;
 
 public class Grid : MonoBehaviour
@@ -9,6 +8,9 @@ public class Grid : MonoBehaviour
 
     [SerializeField]
     private GridRelations gridRelations; //you have to pass in an array of 8, even if some values are null.
+
+    [SerializeField]
+    private bool isStartGrid = false; //this is primarily so that the semi-automatize function will work.
 
     private static Grid startGrid;
 
@@ -29,7 +31,7 @@ public class Grid : MonoBehaviour
             nodeLinks.Add(new DijkstraMap<Grid>.NodeLink() { distance = 1, endObject = g }); //you may modify the distances for some interesting effects. Here, however, a more conventional definition was used.
         }
         gridMap.AddNode(this, nodeLinks);
-        if(startGrid == null)
+        if(isStartGrid)
         {
             startGrid = this;
         }
@@ -93,8 +95,22 @@ public class Grid : MonoBehaviour
         {
             if (g.positionSet)
                 return;
-            g.ExecuteVisualizationProcedure(new Vector3(pos.x + (scale.x / 2) * v.x, pos.y, pos.z + (scale.z / 2) * v.y), scale); //if you change this you can create a non-symmetrical Grid system with this code. Like some grids can be larger than others etc. Of course you'd also have to change the relations and the distances in the pathfinding system. Nonetheless some interesting behaviour may be created.
+            SemiAutomatizeRelations(g, v);
+            g.ExecuteVisualizationProcedure(new Vector3(pos.x + (scale.x) * v.x, pos.y,
+                pos.z + (scale.z) * v.y), scale); //if you change this you can create a non-symmetrical Grid system with this code. Like some grids can be larger than others etc. Of course you'd also have to change the relations and the distances in the pathfinding system. Nonetheless some interesting behaviour may be created.
         });
+    }
+
+    private void SemiAutomatizeRelations(Grid g, Vector2 v) //delete or do not call this if you would like even higher flexibility.
+    {
+        g.gridRelations.relatedGrids[GridRelations.DirectionToID(-v)] = this;
+        for (byte i = 0; i < 8; i++)
+        {
+            byte id = GridRelations.DirectionToID(GridRelations.GetIDDirection(i) - v);
+            if (id == 255)
+                continue;
+            g.gridRelations.relatedGrids[id] = gridRelations.GetGrid(i);
+        }
     }
 
     #endregion
@@ -106,7 +122,6 @@ public class Grid : MonoBehaviour
 public struct GridRelations
 {
     public Grid[] relatedGrids; //clockwise, starting from 0 = upperGrid
-
     public GridRelations(bool created = true)
     {
         relatedGrids = new Grid[8]; 
@@ -128,37 +143,6 @@ public struct GridRelations
     public Grid[] GetGrids()
     {
         return relatedGrids;
-    }
-
-    public Vector2 GetIDDirection(byte directionID)
-    {
-        try
-        {
-            switch (directionID)
-            {
-                case 0:
-                    return new Vector2(0, 1);
-                case 1:
-                    return new Vector2(1, 1).normalized;
-                case 2:
-                    return new Vector2(1, 0);
-                case 3:
-                    return  new Vector2(1, -1).normalized;
-                case 4:
-                    return  new Vector2(0, -1);
-                case 5:
-                    return new Vector2(-1, -1).normalized;
-                case 6:
-                    return  new Vector2(-1, 0);
-                case 7:
-                    return  new Vector2(-1, 1).normalized;
-            }
-        }
-        catch
-        {
-            throw new Exception("the Direction ID provided does not match a direction regarding Grid queries.");
-        }
-        return Vector2.negativeInfinity;
     }
 
     public GridRelation GetGridRelation(byte directionID)
@@ -194,6 +178,170 @@ public struct GridRelations
             action(g.grid, g.direction);
         }
     }
+
+    #region Static Queries
+
+    public static byte NormalizedDirectionToID(Vector2 v)
+    {
+        if (v.x == 0)
+        {
+            if (v.y == 1)
+            {
+                return 0;
+            }
+            else if (v.y == -1)
+            {
+                return 4;
+            }
+        }
+        else if (v.x == 1 || v.x == Mathf.Sqrt(0.5f))
+        {
+            if (v.y == -Mathf.Sqrt(0.5f))
+            {
+                return 3;
+            }
+            else if (v.y == 0)
+            {
+                return 2;
+            }
+            else if (v.y == Mathf.Sqrt(0.5f))
+            {
+                return 1;
+            }
+        }
+        else if (v.x == -1 || v.x == -Mathf.Sqrt(0.5f))
+        {
+            if (v.y == -Mathf.Sqrt(0.5f))
+            {
+                return 5;
+            }
+            else if (v.y == 0)
+            {
+                return 6;
+            }
+            else if (v.y == Mathf.Sqrt(0.5f))
+            {
+                return 7;
+            }
+        }
+
+        return 255;
+    }
+
+    public static Vector2 GetNormalizedIDDirection(byte directionID)
+    {
+        try
+        {
+            switch (directionID)
+            {
+                case 0:
+                    return new Vector2(0, 1);
+                case 1:
+                    return new Vector2(1, 1).normalized;
+                case 2:
+                    return new Vector2(1, 0);
+                case 3:
+                    return new Vector2(1, -1).normalized;
+                case 4:
+                    return new Vector2(0, -1);
+                case 5:
+                    return new Vector2(-1, -1).normalized;
+                case 6:
+                    return new Vector2(-1, 0);
+                case 7:
+                    return new Vector2(-1, 1).normalized;
+            }
+        }
+        catch
+        {
+            throw new Exception("the Direction ID provided does not match a direction regarding Grid queries.");
+        }
+        return Vector2.negativeInfinity;
+    }
+
+    public static Vector2 GetIDDirection(byte directionID)
+    {
+        try
+        {
+            switch (directionID)
+            {
+                case 0:
+                    return new Vector2(0, 1);
+                case 1:
+                    return new Vector2(1, 1);
+                case 2:
+                    return new Vector2(1, 0);
+                case 3:
+                    return new Vector2(1, -1);
+                case 4:
+                    return new Vector2(0, -1);
+                case 5:
+                    return new Vector2(-1, -1);
+                case 6:
+                    return new Vector2(-1, 0);
+                case 7:
+                    return new Vector2(-1, 1);
+            }
+        }
+        catch
+        {
+            throw new Exception("the Direction ID provided does not match a direction regarding Grid queries.");
+        }
+        return Vector2.negativeInfinity;
+    }
+
+    public static byte DirectionToID(Vector2 v, float acceptedMaximumLinearMagnitude = 1f)
+    {
+        acceptedMaximumLinearMagnitude = Mathf.Abs(acceptedMaximumLinearMagnitude);
+        float acceptedMaximumVectorMagnitude = new Vector2(acceptedMaximumLinearMagnitude, acceptedMaximumLinearMagnitude).magnitude;
+        if(v.x == 0)
+        {
+            if(v.y > 0 && v.y <= acceptedMaximumLinearMagnitude)
+            {
+                return 0;
+            }else if(v.y < 0 && v.y >= -acceptedMaximumLinearMagnitude)
+            {
+                return 4;
+            }
+        }
+        else if (v.y == 0)
+        {
+            if (v.x > 0 && v.x <= acceptedMaximumLinearMagnitude)
+            {
+                return 2;
+            }
+            else if (v.x < 0 && v.x >= -acceptedMaximumLinearMagnitude)
+            {
+                return 6;
+            }
+        }
+        else if(v.x == v.y && v.magnitude <= acceptedMaximumVectorMagnitude)
+        {
+            if(v.x < 0)
+            {
+                return 5;
+            }
+            else
+            {
+                return 1;
+            }
+        }else if(v.x == -v.y && v.magnitude <= acceptedMaximumVectorMagnitude)
+        {
+            if(v.x > 0)
+            {
+                return 3;
+            }
+            else
+            {
+                return 7;
+            }
+        }
+
+        return 255;
+    }
+
+
+    #endregion
 
 }
 
